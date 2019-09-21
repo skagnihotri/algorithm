5e1b3386c6ea4227a6bd59f440052f2f4c6bb994
skagnihotri <skagnihotri1@gmail.com>
2019-09-21 14:22:24 +0530

    recursion ques

---
+++ recursion/permutation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void permutation(string str, string ans){
+
+	if (str.length() == 0) {
+		cout<<ans<<endl;
+	}
+
+	for(int i=0; i< str.length(); i++){
+		char ch = str[i];
+		string ros = str.substr(0,i)+str.substr(i+1);
+		// cout<<ros<<endl;
+		permutation(ros, ans+ch);
+	}
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+	permutation("abc", "");
+	return 0;
+}
