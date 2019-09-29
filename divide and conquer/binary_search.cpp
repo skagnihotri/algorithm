d30faf19573c5b36b285b3019b5c287d19fb7a06
skagnihotri <skagnihotri1@gmail.com>
2019-09-29 15:20:36 +0530

    binary_search

--- divide and conquer/binary_search.cpp
+++ divide and conquer/binary_search.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int binary_search(int arr[], int n, int data){
-	int mid = n/2;
+	
 	int start=0;
 	while(start<n) {
+		int mid = (n+start)/2;
 	    if (arr[mid] == data) {
 	    	return mid;
 	    }
 
 	    if (arr[mid] > data){
-	    	n=mid;
+	    	n = mid-1;
 	    }else {
 	    	start = mid +1;
 	    }
@@ -19,9 +20,41 @@ int binary_search(int arr[], int n, int data){
 	return -1;
 }
 
+
+int binary_search_last(int arr[], int n, int data){
+
+	int mid;
+	int start = 0;
+	int flag = 0;
+	while(start<n) {
+	    mid = (n+start)/2;
+	    if (arr[mid] == data) {
+	    	flag = mid;
+	    	start = mid+1;
+	    }
+
+	    if (arr[mid] > data){
+	    	n = mid-1;
+	    }else {
+	    	start = mid +1;
+	    }
+
+	}
+
+	if (flag){
+		return flag;
+	}else{
+		return -1;
+	}
+
+}
+
 int main(int argc, char const *argv[])
 {
-	int a[] = {1,2,3,4,5,6,7,8};
-	cout<<binary_search(a,8,5);
+	int a[] = {1,2,3,4,5,6,5,8};
+	// cout<<binary_search(a,8,5);
+
+	cout<<binary_search_last(a,8,5);
+
 	return 0;
 }
