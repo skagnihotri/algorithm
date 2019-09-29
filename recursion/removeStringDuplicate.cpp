bdfd286b1607fd4a3d8346c092865382e74bc53b
skagnihotri <skagnihotri1@gmail.com>
2019-09-29 12:57:15 +0530

    ques solved removeStringDuplicates

--- recursion/removeStringDuplicate.cpp
+++ recursion/removeStringDuplicate.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using namespace std;
 
-string removeDuplicate(){
+string removeDuplicate(string str){
 	
+	if (str.length() == 0) {
+		return "";
+	}
+
+	char ch = str[0];
+	string ros = str.substr(1);
+	string result = removeDuplicate(ros);
+
+	if (ch == result[0]){
+		return result;		
+	}else {
+		return ch + result;
+	}
+
 }
 
 int main(int argc, char const *argv[])
 {
-	
+	cout<<removeDuplicate("aabsccdbs");
 	return 0;
 }
