bd96f88eafb81ad7b098149dabdffed3a9807a3a
skagnihotri <skagnihotri1@gmail.com>
2019-10-05 12:49:19 +0530

    ques done

--- divide and conquer/rotated_sorted_BinarySearch.cpp
+++ divide and conquer/rotated_sorted_BinarySearch.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int rotated_BinarySearch(int arr[], int start, int n, int data){
+int rotated_BinarySearch(int arr[], int start, int e, int data){
 	
+	if (start>e){
+		return -1;
+	}
 
-	// int start = 0;
-	// int mid = (n+start)/2;
+	int mid = (e+start)/2;
 
-	// if (arr[mid] == data)
-	// {
-	// 	return mid;
-	// }
-	// if (data >= arr[start] and data<= arr[mid]){
-	// 	return rotated_BinarySearch(arr, start, mid, data)
-	// }
+	if (arr[mid]==data){
+		return mid;
+	}
+	if (arr[mid]<arr[start]){
+		if (data>arr[mid] and data<=arr[e]){
+			return rotated_BinarySearch(arr,mid+1,e,data);
+		}else{
+			return rotated_BinarySearch(arr,start,mid-1,data);
+		}
+	}
+
+	if (data>=arr[start] and data<arr[mid]){
+		return rotated_BinarySearch(arr,start,mid-1,data);
+	}
+
+	return rotated_BinarySearch(arr,mid+1,e,data);
 }
 
 int main(int argc, char const *argv[])
 {
 	int arr[] = {5,6,7,1,2,3,4};
-	cout<<rotated_BinarySearch(arr,7,1);
+	cout<<"ANS:-"<<endl;
+	cout<<rotated_BinarySearch(arr,0,7,2)<<endl;
+	cout<<rotated_BinarySearch(arr,0,7,9);
 	return 0;
 }
